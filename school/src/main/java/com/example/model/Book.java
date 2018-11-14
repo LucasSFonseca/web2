88793e76031c6be0d87e4014b9715d7452927076
Lucas Fonseca <luquinhas0805@gmail.com>
2018-11-14 14:35:36 -0300

    first commit

---
+++ school/src/main/java/com/example/model/Book.java
@@ -0,0 +1,140 @@
+package com.example.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
+@Table(name = "book")
+public class Book implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@Column(name="ISBN")
+	private Integer ISBN;
+	
+	@Column(name="numero_paginas")
+	private Integer numeroPaginas;
+	
+	@Column(name="titulo")
+	private String titulo;
+	
+	@Column(name="autor")
+	private String autor;
+	
+	@Column(name="data_publicacao")
+	private String dataP;
+	
+	@Column(name="thumbnail")
+	private String thumbnail;
+	
+	@Column(name="idioma")
+	private String idioma;
+	
+	@Column(name="descricao")
+	private String descricao;
+	
+	@Column(name="media")
+	private float media;
+	
+//	@ManyToOne
+//	@JoinColumn(name="authorId")
+//	private Integer authorId;	
+	
+	@ManyToOne
+	@JoinColumn(name="user_id")
+	private User user; 
+	
+	public Integer getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(Integer iSBN) {
+		ISBN = iSBN;
+	}
+
+	public Integer getNumeroPaginas() {
+		return numeroPaginas;
+	}
+
+	public void setNumeroPaginas(Integer numeroPaginas) {
+		this.numeroPaginas = numeroPaginas;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getDataP() {
+		return dataP;
+	}
+
+	public void setDataP(String dataP) {
+		this.dataP = dataP;
+	}
+
+	public String getThumbnail() {
+		return thumbnail;
+	}
+
+	public void setThumbnail(String thumbnail) {
+		this.thumbnail = thumbnail;
+	}
+
+	public String getIdioma() {
+		return idioma;
+	}
+
+	public void setIdioma(String idioma) {
+		this.idioma = idioma;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public float getMedia() {
+		return media;
+	}
+
+	public void setMedia(float media) {
+		this.media = media;
+	}
+
+//	public Integer getAuthorId() {
+//		return authorId;
+//	}
+//
+//	public void setAuthorId(Integer authorId) {
+//		this.authorId = authorId;
+//	}
+	
+	
+	
+}
+
---
+++ school/src/main/java/com/example/model/User.java
@@ -0,0 +1,102 @@
+package com.example.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "user")
+public class User implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	@Column(name="nome")
+	private String nome;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="login")
+	private String login;
+	
+	@Column(name="password")
+	private String password;
+	
+	@OneToMany(mappedBy="user", cascade=CascadeType.ALL)
+	private List<Book> usuarioTem;
+	
+	@OneToMany(mappedBy="user", cascade=CascadeType.ALL)
+	private List<Book> usuarioQuer;
+
+	public Integer getUserId() {
+		return id;
+	}
+
+	public void setUserId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Book> getUsuarioTem() {
+		return usuarioTem;
+	}
+
+	public void setUsuarioTem(List<Book> usuarioTem) {
+		this.usuarioTem = usuarioTem;
+	}
+
+	public List<Book> getUsuarioQuer() {
+		return usuarioQuer;
+	}
+
+	public void setUsuarioQuer(List<Book> usuarioQuer) {
+		this.usuarioQuer = usuarioQuer;
+	}
+	
+	//@Column(name="")
+
+}
