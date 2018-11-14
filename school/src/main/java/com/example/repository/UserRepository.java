4ca0e8f409eff8c852a66642416e4eeb732c60c4
Diogo Souza <di0g0-dg@Hotmail.com>
2018-11-14 21:13:51 -0200

    Adição dos Repositorios User e Book.

---
+++ school/src/main/java/com/example/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.repository;
+import org.springframework.stereotype.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+	
+}
