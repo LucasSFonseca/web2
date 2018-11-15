b6ba3732ca7259a29e373208e820e09c2f76d0ab
Lucas Fonseca <luquinhas0805@gmail.com>
2018-11-15 20:44:50 -0300

    Commit Teste

--- school/src/main/java/com/example/model/Collection.java
+++ school/src/main/java/com/example/model/Collection.java
@@ -18,6 +18,7 @@ import javax.persistence.Table;
 @Table(name = "collections")
 public class Collection implements Serializable  {
 
+	// Teste
 	private static final long serialVersionUID = 1L;
 	
 	@Id @GeneratedValue(strategy = GenerationType.AUTO)
