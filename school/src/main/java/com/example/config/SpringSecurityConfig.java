7dfb24e8201f9e82bde3cd92e4b165c7fed636a4
Diogo <Diogo@Diogo-PC>
2018-11-28 13:39:57 -0300

    Clear layout criado e opção editar removida do swap.

--- school/src/main/java/com/example/config/SpringSecurityConfig.java
+++ school/src/main/java/com/example/config/SpringSecurityConfig.java
@@ -1,5 +1,8 @@
 package com.example.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,22 +11,26 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter
 {
+	@Autowired
+	DataSource dataSource;
+	
 	/*@Autowired
     private AccessDeniedHandler accessDeniedHandler;*/
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception
 	{
 		auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("USER");
+		//auth.jdbcAuthentication().dataSource(dataSource);
 	}
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception
 	{
-//		http.antMatcher("/**").authorizeRequests().anyRequest().hasRole("USER")
-//				.and().formLogin().loginPage("/login")
-//				/*.failureUrl("login/index.html")*/.loginProcessingUrl("/login")
-//				.permitAll().and().logout()
-//				.logoutSuccessUrl("/home.html");
+		http.antMatcher("/**").authorizeRequests().anyRequest().hasRole("USER")
+				.and().formLogin().loginPage("/login")
+				/*.failureUrl("login/index.html")*/.loginProcessingUrl("/login")
+				.permitAll().and().logout()
+				.logoutSuccessUrl("/home.html");
 	}
 
 }
