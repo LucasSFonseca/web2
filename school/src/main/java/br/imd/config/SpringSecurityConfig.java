59086d19ed0d43e685622afeab6e938e327c7015
OBoyDG <di0g0-dg@hotmail.com>
2018-11-30 14:53:18 -0300

    Adição do template bootstrap "Dashio", login, cadastro e dashboard.

--- school/src/main/java/br/imd/config/SpringSecurityConfig.java
+++ school/src/main/java/br/imd/config/SpringSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -31,26 +32,34 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter
 	@Override
 	public void configure(HttpSecurity http) throws Exception
 	{
-		http.authorizeRequests()
+				http.authorizeRequests()
 				//.anyRequest().hasAnyRole("USER", "ADMIN")
 				.antMatchers("/users/new").permitAll()
 				.antMatchers(HttpMethod.POST, "/users").permitAll()
-				/*.antMatchers("/").hasAnyRole("ADMIN", "USER")
-				.antMatchers("/users").hasAnyRole("ADMIN", "USER")
-				.antMatchers("/users/*").hasAnyRole("ADMIN", "USER")
-				.antMatchers("/books").hasAnyRole("ADMIN", "USER")
-				.antMatchers("/collections").hasAnyRole("ADMIN", "USER")
-				.antMatchers("/wishlists").hasAnyRole("ADMIN", "USER")
-				.antMatchers("/swaps").hasAnyRole("ADMIN", "USER")*/
+//				.antMatchers("/").hasAnyRole("ADMIN", "USER")
+//				.antMatchers("/users").hasAnyRole("ADMIN", "USER")
+//				.antMatchers("/users/*").hasAnyRole("ADMIN", "USER")
+//				.antMatchers("/books").hasAnyRole("ADMIN", "USER")
+//				.antMatchers("/collections").hasAnyRole("ADMIN", "USER")
+//				.antMatchers("/wishlists").hasAnyRole("ADMIN", "USER")
+//				.antMatchers("/swaps").hasAnyRole("ADMIN", "USER")
 				.anyRequest().authenticated()
-				.and().formLogin().loginPage("/login")
-				//.failureUrl("login/index.html")
+				.and().formLogin().loginPage("/login").defaultSuccessUrl("/", true)
+				.failureUrl("/login")
 				.loginProcessingUrl("/login")
-				.permitAll().and().logout()
-				.logoutSuccessUrl("/home.html");
+				.permitAll().and()
+				.logout()
+	            .logoutUrl("/logout")
+	            .logoutSuccessUrl("/login");
 		
 
 		http.csrf().disable();
 	}
-
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+	    web.ignoring().antMatchers( "/img/**",
+	        "/lib/**",
+	        "/css/**");
+	}
 }
